83af3e9851fa0e7ae0e13b743fa8778f971d8079
Vincent Margot <vmargot@advestis-conseil.com>
2018-02-20 12:22:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RIPE',
+      version='0.1',
+      description='RIPE algorithm',
+      author='Vincent Margot',
+      author_email='vincent.margot@hotmail.fr',
+      url='',
+      packages=['RIPE'],
+     )
